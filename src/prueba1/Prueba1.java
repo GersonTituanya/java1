88538a4e580a57dad5bd6105b0f4c9a9af66bd62
Tu Nombre <you@example.com>
2022-11-24 10:46:58 +0100

    Primer commit

---
+++ src/prueba1/Prueba1.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prueba1;
+
+/**
+ *
+ * @author gertit
+ */
+public class Prueba1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+      //  System.out.println("Hola mundo");
+    
+      
+      double op1, op2;
+      char op3,op4;
+      
+      op1=1.34;
+      
+      op2=1.35;
+      
+      op3='a';
+      
+      op4='b';
+      
+      System.out.println("op1 =" + op1 + " op2 =" + op2);
+      
+      System.out.println("op1>op2 = " + ( op1>op2));
+      
+      System.out.println("op1<op2 = " + ( op1<op2));
+      
+      System.out.println("op1==op2 = " + ( op1==op2));
+      
+      System.out.println("op1!=op2 = " + ( op1!=op2));
+      
+      System.out.println("'a'>'b' = " + ( op3>op4));
+      
+      
+    }
+    
+}
